d999d7ee2da460ce8ced616270e7d41c0bed8923
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-09-01 06:39:41 +0000

    Committed!

---
+++ FoodNet/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { assets } from './assets/assets'
+import  Navbar  from './components/navbar/navbar.jsx'
+
+const App = () => {
+  return(
+    <div className='App'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
