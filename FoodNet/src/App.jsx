c6b85cdd04c08101428d39afd44475c21311c58b
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-09-01 07:56:09 +0000

    Committed!

--- FoodNet/src/App.jsx
+++ FoodNet/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { assets } from './assets/assets'
 import  Navbar  from './components/navbar/navbar.jsx'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home.jsx'
+import Cart from './pages/Cart/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
 
 const App = () => {
   return(
     <div className='App'>
       <Navbar />
+      <Routes>
+        <Route path='/' element = { <Home /> }/>
+        <Route path='/cart' element = { <Cart /> }/>
+        <Route path='/place-order' element = { <PlaceOrder /> }/>
+      </Routes>
     </div>
   )
 }
---
+++ FoodNet/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/header/Header.jsx'
+const Home = ()=>{
+    return (
+        <>
+        <Header />
+        </>
+    )
+}
+
+export default Home
