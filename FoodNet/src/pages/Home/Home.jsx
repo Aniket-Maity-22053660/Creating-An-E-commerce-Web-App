dd1e436c8ed985c4d557b1b38f18d08253c1f6fe
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-09-01 15:34:12 +0000

    Committed!

--- FoodNet/src/pages/Home/Home.jsx
+++ FoodNet/src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/header/Header.jsx'
+import Menu from '../../components/menu/Menu.jsx'
 const Home = ()=>{
+    const [category, setCategory] = useState("all")
+
     return (
         <>
         <Header />
+        <Menu category={category} setCategory={setCategory} />
         </>
     )
 }
