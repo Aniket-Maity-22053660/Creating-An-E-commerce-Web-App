a894322478f40328bf414a34c289b522edd81b17
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-09-09 19:36:15 +0000

    Committed!

--- FoodNet/src/pages/Cart/Cart.jsx
+++ FoodNet/src/pages/Cart/Cart.jsx
@@ -1,14 +1,17 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import './Cart.css'
 import {StoreContext} from '../../context/storeContext.jsx'
 
 const Cart = ()=>{
     const {cartItems, food_list, removeCartItems, emptyCartItems } = useContext(StoreContext)
 
+    let total = 0;
+    let delivery = 2;
+
     return(
         <div className='cart'>
             <div className="cart-items">
-                <div className='cart-items-title'>
+                <div className='cart-items-title cart-items-resolution'>
                     <p>Items</p>
                     <p>Title</p>
                     <p>Price</p>
@@ -22,9 +25,10 @@ const Cart = ()=>{
             {
                 food_list.map((item, index)=>{
                     if(cartItems[item._id] > 0){
+                        total += cartItems[item._id] * item.price
                         return(
                             <>
-                            <div className='cart-items-title cart-items-item'>
+                            <div className='cart-items-title cart-items-item cart-item-resolution'>
                                 <img src={item.image} alt='' />
                                 <p>{item.name}</p>
                                 <p>${item.price}</p>
@@ -49,25 +53,25 @@ const Cart = ()=>{
                     <div className='cart-invoice'>
                         <div className="card-total-detail">
                             <p>Subtotal</p>
-                            <p>{0}</p>
+                            <p>${total}</p>
                         </div>
                         <hr />
                         <div className="card-total-detail">
                             <p>Delivery Fee</p>
-                            <p>{2}</p>
+                            <p>${total ? 2 : 0}</p>
                         </div>
                         <hr />
                         <div className="card-total-detail">
                             <b>Total</b>
-                            <b>{0}</b>
+                            <b>${total ? 2 + total : 0}</b>
                         </div>
                         <button>PROCEED TO CHECK OUT</button>
                     </div>
                     <div className='cart-promocode'>
                         <div className='cart-promocode-contents'>
-                            <p>If you have a promo code, Enter it here</p>
+                            <p>If you have a Promo code, Enter it here</p>
                             <div className='cart-promocode-input'>
-                                <input type='text' placeholder='promo code'/>
+                                <input type='text' placeholder='Promo code'/>
                                 <button>Submit</button>
                             </div>
                         </div>
