6a97434578e57a09fceb544cdb693b46dcc0d291
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-09-10 14:56:08 +0000

    Committed!

--- FoodNet/BackEnd/Server.js
+++ FoodNet/BackEnd/Server.js
@@ -11,6 +11,7 @@ const port = 4000
 
 app.use(express.json())
 app.use(cors())
+app.use('/images', express.static('uploads'))
 //Connecting DB
 
 connectDB()
--- FoodNet/BackEnd/controllers/foodController.js
+++ FoodNet/BackEnd/controllers/foodController.js
@@ -2,5 +2,24 @@ import foodModel from "../models/foodModels.js";
 import fs from 'fs'
 
 export const addFood = async (req, res) => {
+    let image_filename = `${req.file.filename}`
+
+    const food = new foodModel({
+        name : req.body.name,
+        description : req.body.description,
+        price : req.body.price,
+        category : req.body.category,
+        image : image_filename
+    })
+
+
+    try{
+        await food.save()
+        res.json({success:true, message:"Food added!"})
+    }catch(e){
+        console.log(e)
+        res.json({success:false, message:"Error"})
+    }
+
 
 }
