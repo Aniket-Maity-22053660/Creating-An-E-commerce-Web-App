3e92f5f5785e04849a9bc833d508f1e170c66082
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-09-10 13:50:09 +0000

    Committed!

---
+++ FoodNet/BackEnd/Server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+//Configuring the app
+
+const app = express()
+const port = 4000
+
+// middleware
+
+app.use(express.json())
+app.use(cors())
+//Connecting DB
+
+connectDB()
+
+// APPI endpoint
+app.use('/api/food', foodRouter)
+
+app.get('/', (req, res)=>{
+    res.send("You are seeing this from the BackEnd")
+})
+
+app.listen(port, ()=>{
+    console.log(`Server started on http://localhost:${port}`)
+})
+
+//mongodb+srv://22053660_db_user:<db_password>@cluster0.1uwlxoz.mongodb.net/?
