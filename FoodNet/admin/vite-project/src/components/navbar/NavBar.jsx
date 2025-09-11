a4f37b46f48a6f4ce0c82f7412119877327a6835
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-09-11 11:02:14 +0000

    Committed!

--- FoodNet/admin/vite-project/src/components/navbar/NavBar.jsx
+++ FoodNet/admin/vite-project/src/components/navbar/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './NavBar.css'
-
+import { Link } from 'react-router-dom'
 import { assets } from '../../assets/assets.js'
 
 const NavBar = ()=>{
     return(
         <div className='navbar'>
-            <img className='logo' src={assets.logo} alt='' />
+
+            <Link to='/' className='logo'><img className='logo' src={assets.logo} alt='' /></Link>
             <img clasName='profile' src={assets.profile_image} alt="" />
         </div>
     )
--- FoodNet/admin/vite-project/src/components/sidebar/SideBar.jsx
+++ FoodNet/admin/vite-project/src/components/sidebar/SideBar.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React, { useState }from 'react'
 import './SideBar.css'
 import { assets } from '../../assets/assets.js'
 import { NavLink } from 'react-router-dom'
 
 const SideBar = ()=>{
+    const [selected, setSelected] = useState({add:false, list:false, order:false})
+
     return(
         <div className='sidebar'>
             <div className='sidebar-options'>
-                <NavLink to='/add' className='sidebar-option'>
+                <NavLink to='/add' className={selected['add'] ? 'selected' : 'sidebar-option'} onClick={()=>setSelected((prev)=>(!prev['add'] ? {['list']:false,['order']:false, ['add'] : true} : {...prev, ['add'] : false}))}>
                     <img src={assets.add_icon} alt=''/>
                     <p>Add Icons</p>
                 </NavLink>
-                <NavLink to='/list'className='sidebar-option'>
+                <NavLink to='/list'className={selected['list'] ? 'selected' : 'sidebar-option'} onClick={()=>(setSelected((prev)=>(!prev['list'] ? {['order']:false,['add']:false, ['list'] : true} : {...prev, ['list']:false})))}>
                     <img src={assets.order_icon} alt=''/>
                     <p>List Items</p>
                 </NavLink>
-                <NavLink to='/order'className='sidebar-option'>
+                <NavLink to='/order'className={selected['order'] ? 'selected' : 'sidebar-option'} onClick={()=>(setSelected((prev)=>(!prev['order'] ? {['list']:false, ['add'] : false,['order'] : true} : {...prev, ['order']:false})))}>
                     <img src={assets.order_icon} alt=''/>
                     <p>Order Items</p>
                 </NavLink>
