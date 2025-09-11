29fe9f5347296c7774f43f8611cc3156572130d8
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-09-11 05:55:31 +0000

    Committed!

--- FoodNet/BackEnd/Server.js
+++ FoodNet/BackEnd/Server.js
@@ -11,7 +11,7 @@ const port = 4000
 
 app.use(express.json())
 app.use(cors())
-app.use('/images', express.static('uploads'))
+app.use('/image', express.static('uploads'))
 //Connecting DB
 
 connectDB()
