b037924ab92e7df34028dca6e70910ea8949086e
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-09-13 12:51:06 +0000

    Committed!

--- FoodNet/src/context/storeContext.jsx
+++ FoodNet/src/context/storeContext.jsx
@@ -1,27 +1,74 @@
 import { createContext, useEffect, useState } from "react";
 import axios from 'axios'
+import { toast } from 'react-toastify'
+
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
-
+    const [token, setToken] = useState("")
     const [cartItems, setCartItems] = useState({})
     const [total, setTotal] = useState(0)
     const [food_list, setFoodList] = useState([])
-    const addCartItems = (itemId) =>{
+    const addCartItems = async (itemId) =>{
         if(!cartItems[itemId]){
             setCartItems((prev)=>({...prev, [itemId] : 1}))
         }else{
             setCartItems((prev)=>({...prev, [itemId] : prev[itemId] + 1}))
         }
+        if(token){
+            try{
+            const response = await axios.post(url+'/cart/add',{itemId}, {headers:{token}})
+            if(response.data.success){
+            console.log(response.data.message)
+            }else{
+                toast.error(response.data.message)
+            }
+            }catch(e){
+                console.log(e)
+            }
+        }else{
+            console.log("Sorry! User not authenticated.")
+        }
     }
-    const removeCartItems = (itemId)=>{
+    const removeCartItems = async (itemId)=>{
         setCartItems((prev)=>({...prev, [itemId] : prev[itemId] - 1}))
+        if(token){
+            try{
+            let response = await axios.post(url+'/cart/remove', {itemId}, {headers:{token}})
+            if(response.data.success){
+                console.log(response.data.message)
+            }else{
+                toast.error(response.data.message)
+            }
+            }catch(e){
+                console.log(e)
+            }
+        }else{
+            console.log("Sorry! User is not authenticated.")
+        }
+    }
+    const loadCartItems = async (token)=>{
+        if(token){
+            try{
+                let response = await axios.get(url+'/cart/get', {headers:{token}})
+                if(response.data.success){
+                    console.log("Resource fetched successfully!")
+                    setCartItems(response.data.cartData)
+                }else{
+                    toast.error(response.data.message)
+                }
+            }catch(e){
+                console.log(e)
+            }
+        }else{
+            console.log("Sorry! User is not authenticated")
+        }
     }
     const emptyCartItems = (itemId)=>{
         setCartItems((prev)=>({...prev, [itemId]: 0}))
     }
     const url = "http://localhost:4000/api"
-    const [token, setToken] = useState("")
+ 
     useEffect(()=>{
         console.log(cartItems)
     }, [cartItems])
@@ -49,6 +96,9 @@ const StoreContextProvider = (props) => {
     }
     loadData()
     }, [])
+    useEffect(()=>{
+        loadCartItems(token)
+    }, [token])
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
